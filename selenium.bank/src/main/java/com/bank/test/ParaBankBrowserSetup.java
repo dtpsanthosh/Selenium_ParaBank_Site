e12bbceb15c8ec17851ba1de71febeb62241d368
SANTHOSH <SANTHOSH@DESKTOP-9SFNIPP>
2023-01-20 15:59:01 +0530

    selenium_parabank

---
+++ selenium.bank/src/main/java/com/bank/test/ParaBankBrowserSetup.java
@@ -0,0 +1,39 @@
+package com.bank.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.bank.webelements.ParaBankWebElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ParaBankBrowserSetup {
+	WebDriver driver;
+	ParaBankWebElements webElements;
+
+	@Parameters({ "browser", "url" })
+	@BeforeClass
+	public void setup(String browser, String url) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		webElements = new ParaBankWebElements(driver);
+		driver.manage().window().maximize();
+		driver.get(url);
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
---
+++ selenium.bank/src/main/java/com/bank/webelements/ParaBankWebElements.java
@@ -0,0 +1,127 @@
+package com.bank.webelements;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ParaBankWebElements {
+	WebDriver driver;
+
+	public ParaBankWebElements(WebDriver driver1) {
+		this.driver = driver1;
+		PageFactory.initElements(driver1, this);
+	}
+
+	@FindBy(linkText = "Register")
+	WebElement register;
+
+	public WebElement getregister() {
+		return register;
+	}
+
+	@FindBy(id = "customer.firstName")
+	WebElement firstName;
+
+	public WebElement getfirstName() {
+		return firstName;
+	}
+
+	@FindBy(id = "customer.lastName")
+	WebElement lastName;
+
+	public WebElement getlastName() {
+		return lastName;
+	}
+
+	@FindBy(id = "customer.address.street")
+	WebElement street;
+
+	public WebElement getStreet() {
+		return street;
+	}
+
+	@FindBy(id = "customer.address.city")
+	WebElement city;
+
+	public WebElement getcity() {
+		return city;
+	}
+
+	@FindBy(id = "customer.address.state")
+	WebElement state;
+
+	public WebElement getstate() {
+		return state;
+	}
+
+	@FindBy(id = "customer.address.zipCode")
+	WebElement zipcode;
+
+	public WebElement getzipcode() {
+		return zipcode;
+	}
+
+	@FindBy(id = "customer.phoneNumber")
+	WebElement phoneNumber;
+
+	public WebElement getphoneNumber() {
+		return phoneNumber;
+	}
+
+	@FindBy(id = "customer.ssn")
+	WebElement ssn;
+
+	public WebElement getssn() {
+		return ssn;
+	}
+
+	@FindBy(id = "customer.username")
+	WebElement username;
+
+	public WebElement getusername() {
+		return username;
+	}
+
+	@FindBy(id = "customer.password")
+	WebElement password;
+
+	public WebElement getpassword() {
+		return password;
+	}
+
+	@FindBy(id = "repeatedPassword")
+	WebElement confirmPassword;
+
+	public WebElement getconfirmPassword() {
+		return confirmPassword;
+	}
+
+	@FindBy(xpath = "//input[@value = 'Register']")
+	WebElement registrationButton;
+
+	public WebElement getregistrationButton() {
+		return registrationButton;
+	}
+
+	@FindBy(name = "username")
+	WebElement loginusername;
+
+	public WebElement getloginusername() {
+		return loginusername;
+	}
+
+	@FindBy(name = "password")
+	WebElement loginPassword;
+
+	public WebElement getloginPassword() {
+		return loginPassword;
+	}
+
+	@FindBy(xpath = "//input[@type = 'submit']")
+	WebElement login;
+
+	public WebElement getlogin() {
+		return login;
+	}
+}
