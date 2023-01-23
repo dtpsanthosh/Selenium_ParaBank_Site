4005c7cc8b47a7f274e50f7011bf8caf384d4f24
SANTHOSH <SANTHOSH@DESKTOP-9SFNIPP>
2023-01-23 14:33:24 +0530

    a

--- selenium.bank/src/main/java/com/bank/test/ParaBankTest.java
+++ selenium.bank/src/main/java/com/bank/test/ParaBankTest.java
@@ -2,11 +2,12 @@ package com.bank.test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class ParaBankTest extends ParaBankBrowserSetup {
 
-	@Test(priority = 0, enabled = true)
+	@Test(priority = 0, enabled = false)
 	public void register() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.getTitle();
@@ -25,10 +26,31 @@ public class ParaBankTest extends ParaBankBrowserSetup {
 		webElements.getconfirmPassword().sendKeys("123");
 		webElements.getregistrationButton().click();
 	}
-	@Test(priority = 1,enabled = true)
+
+	@Test(priority = 1, enabled = true)
 	public void login() {
-		webElements.getloginusername().sendKeys("santhoshkumar");
-		webElements.getloginPassword().sendKeys("123");
+		webElements.getloginusername().sendKeys("22");
+		webElements.getloginPassword().sendKeys("22");
 		webElements.getlogin().click();
+	}
+
+	@Test(priority = 2, enabled = true)
+	public void operations() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		webElements.getopenNewAccount().click();
+		Select selecttypeOfAccount = new Select(webElements.gettypeOfAccount());
+		selecttypeOfAccount.selectByIndex(0);
+		/*
+		 * Select selectfromAccountID = new Select(webElements.getfromAccountId());
+		 * selectfromAccountID.selectByIndex(0);
+		 */
+		webElements.getopenNewAccount1initial().click();
+		Thread.sleep(2000); // if we put thread.sleep it will override
+		webElements.getnewAccountID().click();
+		Select month = new Select(webElements.getmonth());
+		month.selectByValue("April");
+		Select transactionType = new Select(webElements.gettransactionType());
+		transactionType.selectByValue("Credit");
+		webElements.getGo().click();
 	}
 }
